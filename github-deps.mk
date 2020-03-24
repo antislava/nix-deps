00f9368724f9450bb78a98c66965201b1b1b2da3
Anti Slava <antislava@gmail.com>
2020-03-24 19:49:02 +0100

    Repo commit guards

--- github-deps.mk
+++ github-deps.mk
@@ -29,11 +29,12 @@ $(TRGTS):
 
 # make NOCHECK=true list-targets
 ifneq ($(NOCHECK), true)
-GIT_STATUS := $(shell git diff --exit-code --shortstat 1>/dev/null; echo $$?)
-#             $(shell git diff --exit-code --shortstat 1>&2; echo $$?)
+GIT_STATUS_CMD := git diff --exit-code --shortstat
+GIT_STATUS := $(shell $(GIT_STATUS_CMD) 1>/dev/null; echo $$?)
+#             $(shell $(GIT_STATUS_CMD) 1>&2       ; echo $$?)
 # https://stackoverflow.com/questions/5139290/how-to-check-if-theres-nothing-to-be-committed-in-the-current-branch
 ifneq ($(GIT_STATUS), 0)
-  $(error Git repo is not clean: $(shell git diff --exit-code --shortstat))
+  $(error Git repo is not clean: $(shell $(GIT_STATUS_CMD)))
 endif
 endif
 
@@ -60,3 +61,4 @@ all-default-nix: $(DEFAULT_NIXS)
 # Overwriting default.nix each time the template changes! Not totally sure it is optimal...
 %/default.nix: ./fetchByGithubJson.nix
 	cp ./fetchByGithubJson.nix $(@D)/default.nix
+
